d0acd080435d2e31b41f5d9bc504009005fe5b63
Jonathan <jrhaccount@yahoo.com>
2015-01-19 18:11:39 -0600

    updated

---
+++ encode.js
@@ -0,0 +1,75 @@
+// Example.....
+// function encode(phrase){
+//     var result = "";
+//     // .. for each character in phrase
+//     for(var i = 0; i < phrase.length; i++){
+//         // get the charCode if letter at index i
+//         var cc = phrase[i].charCodeAt(0);
+//         // add 13 to the letter's charCode
+//         result += String.fromCharCode(cc+13);
+
+//     }
+//     return result;
+// }
+
+// console.log(encode("hello")); //---> "uryyb"
+
+// function decode(phrase){
+// 	var result = "";
+// 	for(var i = 0; i < phrase.length; i++){
+//         // get the charCode if letter at index i
+//         var cc = phrase[i].charCodeAt(0);
+//         // add 13 to the letter's charCode
+//         result += String.fromCharCode(cc-13);
+    
+//     }
+//     return result;
+// }
+
+// console.log(decode("uryyb")); //---> "hello"
+
+// //Practice....
+
+function encode(phrase){
+    var result = "";
+    // .. for each character in phrase
+    for(var i = 0; i < phrase.length; i++){
+        // get the charCode if letter at index i
+        var cc = phrase[i].charCodeAt(0);
+    
+        // add 13 to the letter's charCode
+        if ((cc+13) > 122){
+            cc2 = (cc+13) - 26
+         result += String.fromCharCode(cc2);
+                            }
+                            else{
+                             result += String.fromCharCode(cc+13);
+                            }
+    }
+    return result;
+}
+
+console.log(encode("hello")); //---> "uryyb"
+
+function decode(phrase){
+    var result = "";
+    for(var i = 0; i < phrase.length; i++){
+        // get the charCode if letter at index i
+        var cc = phrase[i].charCodeAt(0);
+        // add 13 to the letter's charCode
+      
+            if ((cc-13) < 97){
+            cc2 = (cc-13) + 26
+         result += String.fromCharCode(cc2);
+                            }
+                            else{
+                            result += String.fromCharCode(cc-13);
+                            }
+                            
+                            
+    }
+    return result;
+}
+
+console.log(decode("uryyb")); //---> "hello"
+
